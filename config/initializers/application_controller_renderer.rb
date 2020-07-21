2ba7807cc9ee84eeac74fda1d164a20e179dee5a
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-07-21 18:35:22 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '76097184524708f34fdc289e65984a9859bfd2d459588d3ef2c7ac40f2036b6b66d0c3e6207e976a1eeec3367ad68410bff9f1a63b70ab7a2ed21b9f0a208abe'
